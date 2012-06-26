3c7d3a203419db3b5148d04c6cd89a69a02139e0
Carles Bruguera <carlesba@gmail.com>
2012-06-26 13:11:02 +0200

    Homepage

--- js/script.js
+++ js/script.js
@@ -5,6 +5,16 @@
 
 
 
+$('.collapse')
+  .on('shown', function(event) {
+    parent = $(event.target).closest('.accordion-group')
+    $(parent).toggleClass('visible', true)
+   })
+  .on('hidden', function(event) {
+    parent = $(event.target).closest('.accordion-group')
+    $(parent).toggleClass('visible', false)
+   })
+
 
 
 
