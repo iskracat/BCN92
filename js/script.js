853cfdb3deed053d48138485e20b04d7247a1bfc
Carles Bruguera <carlesba@gmail.com>
2012-06-27 08:43:16 +0200

    Change header css on browsing sections

--- js/script.js
+++ js/script.js
@@ -6,13 +6,20 @@
 
 
 $('.collapse')
-  .on('shown', function(event) {
+  .on('show', function(event) {
     parent = $(event.target).closest('.accordion-group')
     $(parent).toggleClass('visible', true)
+    homevisible = $('#home').hasClass('visible')
+    console.log(homevisible)
+    $('#accordion2').toggleClass('athome', homevisible)
+
    })
-  .on('hidden', function(event) {
+  .on('hide', function(event) {
     parent = $(event.target).closest('.accordion-group')
     $(parent).toggleClass('visible', false)
+    homevisible = $('#home').hasClass('visible')
+    console.log(homevisible)
+    $('#accordion2').toggleClass('athome', homevisible)
    })
 
 
