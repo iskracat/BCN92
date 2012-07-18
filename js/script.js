e22ffa74fe435d2daafc50d2b47191c02e77a403
Ramon Navarro Bosch <ramon.nb@gmail.com>
2012-07-18 18:41:55 +0200

    ara

--- js/script.js
+++ js/script.js
@@ -521,8 +521,8 @@ function recalculatePinPosition(pin) {
 
 function getAvailableSize(visibleSection) {
     var total = 0
-    var headers_at_home = 200
-    var headers_at_sections = 130
+    var headers_at_home = 198
+    var headers_at_sections = 105
 
     //216
 
